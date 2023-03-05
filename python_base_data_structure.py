ce749008023591af1499c245031349627963ec8b
Kassio Wallace <kassiowl34@gmail.com>
2023-03-05 13:09:22 -0300

    var name changed

--- python_base_data_structure.py
+++ python_base_data_structure.py
@@ -1,14 +1,14 @@
 def data_structure():
 
-    list_var = [12,"banana", 5.3, 9,1,2,3] #list_var    Ordered, changeable, duplicates
+    L = [12,"banana", 5.3, 9,1,2,3] #L    Ordered, changeable, duplicates
     T = (12, "banana", 5.3, 12) #Tuple  Ordered, unchangeable, duplicates
     S = {12, "banana", 5.3} #Set    Unordered, addable/removable, no duplicate
     D = {"Val": 12, "name": "Ban"} #Dict    Unordered, changeable, no duplicate
 
 
-    list_var[0] = 15
+    L[0] = 15
 
-    del list_var[1] #delete "banana"
+    del L[1] #delete "banana"
 
     # T[2] = Return Error 5 TypeError: 'tuple' object does not support item assignment
 
@@ -25,9 +25,9 @@ def data_structure():
     #print(S[0])    Return Error TypeError: 'set' object is not subscriptable
 
 
-    list_var.pop() #Remove last item element
+    L.pop() #Remove last item element
 
-    # list_var.add(25)   #Return Error list_var object has no attribute add
+    # L.add(25)   #Return Error L object has no attribute add
 
 
     #print(D[0])    #Return KeyError: 0
@@ -39,19 +39,20 @@ def data_structure():
 
    # D = D+D2   Return TypeError: unsupported operand type(s) for +: 'dict' and 'dict'
 
-    L2 = list_var #any changes in L2, L1 will be also affected, that's because it's pointed to the same memory address 
-    del L2[-1] # Deleta 5.3 de L2 e list_var
+    L2 = L #any changes in L2, L1 will be also affected, that's because it's pointed to the same memory address 
+    del L2[-1] # Deleta 5.3 de L2 e L
 
-    L3 = list_var.copy()   #Create a actually a copy of the variable
+    L3 = L.copy()   #Create a actually a copy of the variable
    
-    del L3[0]  #Now i can delete without affect list_var 
+    del L3[0]  #Now i can delete without affect L 
 
-    #L3 = list_var[0:1]    It's already a copy
+    #L3 = L[0:1]    It's already a copy
 
 
-    list_var.append(55)    #Add 55 in the end of the list_var
+    L.append(55)    #Add 55 in the end of the L
 
-    return {"This_data_structure": "Dict","list_var(list_var)": list_var, "list_var(L2)":L2, "list_var(L3)": L3, "Tuple": T, "Set": S, "Dict": D}
+    print(L[::2])
+    return {"This_data_structure": "Dict","L(L)": L, "L(L2)":L2, "L(L3)": L3, "Tuple": T, "Set": S, "Dict": D}
 
 
 
